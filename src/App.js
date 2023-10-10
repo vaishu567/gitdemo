1bcc4c24a3c2fb5cbe1c78bbdd210ca74fdd2306
vaishu567 <rslv3vaishu567@gmail.com>
2023-10-10 21:54:27 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Form from "./components/Form";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
       <Form />
     </>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,41 +3,37 @@ import React from "react";
 const Form = () => {
   return (
     <>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
+      <div className="container my-3">
+        <div className="row justify-content-center">
+          <div
+            className="col-md-10"
+            style={{
+              border: "2px solid",
+              borderRadius: "50px",
+              padding: "30px",
+            }}
+          >
+            <form>
+              <div class="mb-3">
+                <h1>Todo App</h1>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="todo"
+                  placeholder="Try typing: Do Exercise"
+                />
+              </div>
+              <button
+                type="submit"
+                class="btn btn-primary w-100"
+                style={{ borderRadius: "50px", padding: "5px" }}
+              >
+                Submit
+              </button>
+            </form>
           </div>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </>
   );
 };
